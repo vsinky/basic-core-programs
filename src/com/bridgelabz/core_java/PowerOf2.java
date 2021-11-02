602173951e14934aa211129584b03ded6dbc83d1
vsinky <sinkykumariverma@gmail.com>
2021-11-02 18:39:05 +0530

    print a table of the power of 2

--- src/com/bridgelabz/core_java/PowerOf2.java
+++ src/com/bridgelabz/core_java/PowerOf2.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.core_java;
 
+import java.util.Scanner;
+
 public class PowerOf2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number:  ");
+		// INITIALIZATION
+		int num, mul = 1;
+		num = sc.nextInt();
+
+		// CONDITION CHECK
+		if (num > 31) {
+			System.out.println("value of number should be less than 31");
+			return;
+		}
+
+		// COMPUTATION
+		else {
+			System.out.println(mul);
+			for (int j = 1; j <= num; j++) {
+				mul = mul * 2;
+				System.out.println(mul);
+			}
+		}
+
+	}
 
 }
