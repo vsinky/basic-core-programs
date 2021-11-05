738f8e43872d931fdcecc8f00c8fae3ff82fb4b2
vsinky <sinkykumariverma@gmail.com>
2021-11-05 17:59:43 +0530

    Added the triple

---
+++ src/com/bridgelabz/funcation/SumOfZero.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.funcation;
+
+public class SumOfZero {
+	static void findTriplets(int[] arr, int n) {
+		boolean found = false;
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				for (int k = j + 1; k < n; k++) {
+					if (arr[i] + arr[j] + arr[k] == 0) {
+						System.out.print(arr[i]);
+						System.out.print(" ");
+						System.out.print(arr[j]);
+						System.out.print(" ");
+						System.out.print(arr[k]);
+						System.out.print("\n");
+						found = true;
+					}
+				}
+			}
+		}
+
+		// If no triplet with 0 sum found in array
+		if (found == false)
+			System.out.println(" not exist ");
+
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 0, -1, 2, -3, 1 };
+		int n = arr.length;
+		findTriplets(arr, n);
+
+	}
+}
